60f1cebcb831856e96e7fd13ca73968353980182
Sztwiorok <rafal.sztwiorok@gmail.com>
2026-08-25 09:52:06 +0200

    module and pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120_000,
   },
 });
